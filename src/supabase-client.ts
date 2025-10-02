138d908f8cc6fee77f7e38a31de1356d5f54c160
Maurii <management.maurii@outlook.com>
2025-10-02 22:28:23 +0200

    t

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,7 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+console.log(import.meta.env.VITE_SUPABASE_URL);
+console.log(import.meta.env.VITE_SUPABASE_KEY);
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Supabase URL and Key are required. Check your env variables."
+  );
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
